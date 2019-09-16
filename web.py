e779da434b3aa07589d2b9c4c23240e99c5c3156
wan <851798349@qq.com>
2019-09-16 19:11:33 +0800

    1.网页模板修改了表单action，app.route()参数也对应进行修改 2.新增注册函数，接收前端数据并处理 3.实现会话（每个函数都加入了会话相关代码），登录后网页页眉会记录用户名 4.实现用户退出登录功能 5.本次提交后，必须登录后才可以进入系统管理界面 ”

--- web.py
+++ web.py
@@ -1,20 +1,27 @@
 #!/etc/bin/python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, redirect, session
 import good_operation, goods, users
 
 app = Flask(__name__)
+app.secret_key = os.urandom(16)
 
-@app.route("/home.html", methods=["GET", "POST"])
-def find():
+@app.route('/')
+def index():
+    return redirect('/login')
+
+@app.route("/home", methods=["GET", "POST"])
+def home_find():
     '''
     功能：查询商品（查找所有、ID查找、名称查找、）
     返回值：模板home.html
     
     '''
+    username = session.get("username")
     if request.method == "GET":
-        return render_template("home.html")
+        return render_template("home.html", username=username)
     elif request.method == "POST":
         f = request.form.get("selected")
         w = request.form.get("way")
@@ -31,18 +38,19 @@ def find():
             g.set_gname(w)
             gs = good_operation.get_byname(g)
             gs = (gs, )
-        return render_template("home.html", gs=gs)
+        return render_template("home.html", gs=gs, username=username)
 
 
-@app.route("/home_add.html", methods=["GET", "POST"])
+@app.route("/home_add", methods=["GET", "POST"])
 def home_add():
     '''
     功能：增加商品
     返回值：模板home_add.html
     
     '''
+    username = session.get("username")
     if request.method == "GET":
-        return render_template("home_add.html")
+        return render_template("home_add.html", username=username)
     elif request.method == "POST":
         gname = request.form.get("gname")
         gprice = request.form.get("gprice")
@@ -54,17 +62,18 @@ def home_add():
         good_operation.add_good(g)
         gs = good_operation.get_byname(g)
         gs = (gs, )
-        return render_template("home_add.html", gs = gs)
+        return render_template("home_add.html", gs = gs, username=username)
 
-@app.route("/home_upd.html", methods=["GET", "POST"])
+@app.route("/home_upd", methods=["GET", "POST"])
 def home_upd():
     '''
     功能：增加商品
     返回值：模板home_upd.html
     
     '''
+    username = session.get("username")
     if request.method == "GET":
-        return render_template("home_upd.html")
+        return render_template("home_upd.html", username=username)
     elif request.method == "POST":
         gid = request.form.get("gid")
         gname = request.form.get("gname")
@@ -78,17 +87,18 @@ def home_upd():
         good_operation.update_good(g)
         gs = good_operation.get_byid(g)
         gs = (gs, )
-        return render_template("home_upd.html", gs = gs)
+        return render_template("home_upd.html", gs = gs, username=username)
 
-@app.route("/home_del.html", methods=["GET", "POST"])
+@app.route("/home_del", methods=["GET", "POST"])
 def home_del():
     '''
     功能：增加商品
     返回值：模板home_del.html
     
     '''
+    username = session.get("username")
     if request.method == "GET":
-        return render_template("home_del.html")
+        return render_template("home_del.html", username=username)
     elif request.method == "POST":
         gid = request.form.get("gid")
         g = goods.good()
@@ -96,10 +106,10 @@ def home_del():
         gs = good_operation.get_byid(g)
         good_operation.delete_good(g)
         gs = (gs, )
-        return render_template("home_del.html", gs = gs)
+        return render_template("home_del.html", gs = gs, username=username)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
         return render_template("login.html")
@@ -108,9 +118,23 @@ def login():
         password = request.form.get("password")
         row = users.login_check(username, password)
         if row:
-            return render_template("home.html")
+            session["username"] = username
+            return render_template("home.html", username=username)
         else:
-            return "登陆失败"
+            return render_template("login.html", feedback="登录失败！")
+
+@app.route("/reg", methods=["GET", "POST"])
+def reg():
+    if request.method == "GET":
+        return render_template("reg.html")
+    elif request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        phone = request.form.get("phone")
+        email = request.form.get("email")
+        print(username, password, phone, email)
+        users.user_reg(username, password, phone, email)
+        return render_template("reg.html", feedback="注册成功！")
 
 
 
