95453dc873b76250afa16943ffd069766e23cfd6
wan <851798349@qq.com>
2019-09-17 15:43:04 +0800

    重写，新增两个校验函数分别用来校验登录和注册输入信息是否合法

--- static/js/login.js
+++ static/js/login.js
@@ -1,23 +1,75 @@
-function login() {
+"use strict"
 
-    var username = document.getElementById("username");
-    var pass = document.getElementById("password");
+var username;
+var password;
 
-    if (username.value == "") {
-
-        alert("请输入用户名");
-
-    } else if (pass.value  == "") {
-
-        alert("请输入密码");
-
-    } else if(username.value == "admin" && pass.value == "123456"){
-
-        window.location.href="index.html";
-
-    } else {
-
-        alert("请输入正确的用户名和密码！")
-
+function login_verify() {
+    username = $("#username").val();
+    password = $("#password").val();
+    if (username == "") {
+        $("#username").attr("placeholder", "用户名不能为空");
+        return false;
     }
+    else if (username.length < 5 || username.length > 16) {
+        $("#username").val("");
+        $("#username").attr("placeholder", "用户名长度为5~16");
+        return false;
+    }
+    else if (password == "") {
+        $("#password").attr("placeholder", "密码不能为空");
+        return false;
+    }
+    else if (password.length < 5 || password.length > 16) {
+        $("#password").val("");
+        $("#password").attr("placeholder", "密码长度为5~16");
+        return false;
+    }
+
+    return true;
+}
+
+
+function reg_verify() {
+    username = $("#username").val();
+    password = $("#password").val();
+    var password1 = $("#password1").val();
+    var phone = $("#phone").val();
+    var email = $("#email").val();
+    if (username == "") {
+        $("#username").attr("placeholder", "用户名不能为空");
+        return false;
+    }
+    else if (username.length < 5 || username.length > 16) {
+        $("#username").val("");
+        $("#username").attr("placeholder", "用户名长度为5~16");
+        return false;
+    }
+    else if (password == "") {
+        $("#password").attr("placeholder", "密码不能为空");
+        return false;
+    }
+    else if (password.length < 5 || password.length > 16) {
+        $("#password").val("");
+        $("#password").attr("placeholder", "密码长度为5~16");
+        return false;
+    }
+    else if (password1 !== password) {
+        $("#password1").val("");
+        $("#password1").attr("placeholder", "密码不一致");
+        return false;
+    }
+    else if (phone == "") {
+        $("#phone").attr("placeholder", "手机号不能为空")
+        return false;
+    }
+    else if (phone.length !== 11) {
+        $("#phone").val("");
+        $("#phone").attr("placeholder", "手机号不合法")
+        return false;
+    }
+    else if (email == "") {
+        $("#email").attr("placeholder", "邮箱不能为空")
+        return false;
+    }
+    return true;
 }
