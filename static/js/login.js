95efc81f8a88f62dedb76d1ff345fee97e44bbfc
wan <851798349@qq.com>
2019-09-28 09:23:35 +0800

    大致未改动

--- static/js/login.js
+++ static/js/login.js
@@ -1,11 +1,8 @@
-"use strict"
-
-var username;
-var password;
+"use strict";
 
 function login_verify() {
-    username = $("#username").val();
-    password = $("#password").val();
+    var username = $("#username").val();
+    var password = $("#password").val();
     if (username == "") {
         $("#username").attr("placeholder", "用户名不能为空");
         return false;
@@ -29,47 +26,4 @@ function login_verify() {
 }
 
 
-function reg_verify() {
-    username = $("#username").val();
-    password = $("#password").val();
-    var password1 = $("#password1").val();
-    var phone = $("#phone").val();
-    var email = $("#email").val();
-    if (username == "") {
-        $("#username").attr("placeholder", "用户名不能为空");
-        return false;
-    }
-    else if (username.length < 5 || username.length > 16) {
-        $("#username").val("");
-        $("#username").attr("placeholder", "用户名长度为5~16");
-        return false;
-    }
-    else if (password == "") {
-        $("#password").attr("placeholder", "密码不能为空");
-        return false;
-    }
-    else if (password.length < 5 || password.length > 16) {
-        $("#password").val("");
-        $("#password").attr("placeholder", "密码长度为5~16");
-        return false;
-    }
-    else if (password1 !== password) {
-        $("#password1").val("");
-        $("#password1").attr("placeholder", "密码不一致");
-        return false;
-    }
-    else if (phone == "") {
-        $("#phone").attr("placeholder", "手机号不能为空")
-        return false;
-    }
-    else if (phone.length !== 11) {
-        $("#phone").val("");
-        $("#phone").attr("placeholder", "手机号不合法")
-        return false;
-    }
-    else if (email == "") {
-        $("#email").attr("placeholder", "邮箱不能为空")
-        return false;
-    }
-    return true;
-}
+
